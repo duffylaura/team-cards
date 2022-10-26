fbffe0ac45d6b302f784216b14a41758267e63b8
Coffee Pawz <lauralynneduffy@gmail.com>
2022-10-26 12:53:54 -0400

    Laid out framework of how I want code to be; no js errors but prompts not loading

--- index.js
+++ index.js
@@ -3,24 +3,78 @@ const inquirer = require('inquirer');
 const manager = require ('./lib/01manager'); 
 const engineer = require ('./lib/02engineer'); 
 const intern = require ('./lib/03intern');
-const promptQuestions = require('./lib/prompt-questions'); 
+const managerQuestions = require('./lib/prompt-questions'); 
+const engineerQuestions = require('./lib/prompt-questions');
+const internQuestions = require('./lib/prompt-questions');
+const whatNextQuestions = require('./lib/prompt-questions');
+const generateHTML = require('./lib/generateHTML');
+
 
 const cards = []; 
 
-inquirer.prompt(managerQuestions)
+function init () {
+    inquirer.prompt(managerQuestions)
+    .then((data) => {
+        const managerEntry = new manager (data.name, data.id, data.email, data.officeNumber);
+        cards.push(managerEntry);
 
-.then((data) => {
-    if (data.select === "engineer")
-    {inquirer.prompt(engineerQuestions)}
-    if (data.select === "intern")
-    {inquirer.prompt(internQuestions)}
-}
-)
+        if (data.select === "engineer")
+            {inquirer.prompt(engineerQuestions)
+            
+                .then((data) => {
+                    const engineerEntry = new engineer (data.name, data.id, data.email, data.github);
+                    cards.push(engineerEntry);
+                })
 
+            whatNext(); 
+            }
 
+        else if (data.select === "intern")
+            {inquirer.prompt(internQuestions)
+                
+                .then((data)=>{
+                    const internEntry = new intern (data.name, data.id, data.email, data.school);
+                    cards.push(internEntry);
+                }) 
+            whatNext();
+            }
+    }
+)};
 
+const whatNext = () => {   
+        inquirer.prompt(whatNextQuestions) // if engineer, if intern, if end 
+        .then((data) => {
 
+            if (data.whatNext === "engineer")
+                {inquirer.prompt(engineerQuestions)
+                
+                    .then((data) => {
+                        const engineerEntry = new engineer (data.name, data.id, data.email, data.github);
+                        cards.push(engineerEntry);
+                    })
 
+                whatNext(); 
+                }
 
+            else if (data.whatNext === "intern")
+            {inquirer.prompt(internQuestions)
+                
+                .then((data)=>{
+                    const internEntry = new intern (data.name, data.id, data.email, data.school);
+                    cards.push(internEntry);
+                }) 
+            whatNext();
+            }
 
+            else if (data.whatNext === "end")
+            {
+                const output = generateHTML(cards);
 
+                fs.writeFile('./dist/index.html', output, (err)=>          
+                err ? console.log(err): console.log('Created your team contact cards successfully.')
+            );
+            }
+        }) 
+};
+
+init();
